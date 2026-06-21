276505f554a939d654494177064ed1537e997c1d
Satvikpatil 1582 <patilsatvik30@gmail.com>
2026-06-21 20:13:21 +0530

    Initial commit

---
+++ src/componentss/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar=()=>{
+    return(
+        <div>
+            <div className="nav">
+                Lorem, ipsum.
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
